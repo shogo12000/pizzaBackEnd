0e685a63f2d75d326777abc80843e76052b5b473
sidney <shogo12000@hotmail.com>
2025-05-31 21:56:19 -0700

    fix Mongo connect

--- server.js
+++ server.js
@@ -28,10 +28,7 @@ app.use(cors({
   credentials: true // se  for usar cookies ou auth com headers
 }));
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected"))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB error:", err));
 
 app.use("/api", require("./routes/auth"));
